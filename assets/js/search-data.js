b10e28d56079040ebb2146ee6eb451a0ecdbfed2
YahanLu <yahanlu@tudelft.nl>
2026-08-24 23:13:39 +0000

    Deploying to gh-pages from @ YahanLu/YahanLu.github.io@82c9d50c4c491ca08e4d861f64df43214309a324 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,6 +44,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/presentations/";
           },
+        },{id: "nav-contact",
+          title: "Contact",
+          description: "Contact Yahan Lu by email.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/contact/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
